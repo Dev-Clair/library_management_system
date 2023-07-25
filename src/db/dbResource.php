cac26a2f62b138d604feef72ad5f9955bd725bfd
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-07-25 15:01:24 +0100

    feat: modifying dbResource.php

--- src/db/dbResource.php
+++ src/db/dbResource.php
@@ -4,32 +4,6 @@ declare(strict_types=1);
 
 namespace db;
 
-//// Without MVC Design pattern
-// use Dotenv\Dotenv;
-// use db\Connection\DbConn;
-// use db\Connection\DbTable;
-// use db\Connection\DbTableOp;
-
-// $dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
-// $dotenv->load();
-
-// /******* Create/Drop/Truncate/Alter Table ******/
-// function tableConnection(string $databaseName): DbTable
-// {
-//     $conn = new DbConn(driver: $_ENV["DSN_DRIVER"], serverName: $_ENV["DATABASE_HOSTNAME"], userName: $_ENV["DATABASE_USERNAME"], password: $_ENV["DATABASE_PASSWORD"], database: $databaseName);
-//     $conn = new DbTable($conn->getConnection());
-//     return $conn;
-// }
-
-// /******* Table Read and Write Operations ******/
-// function tableOpConnection(string $databaseName): DbTableOp
-// {
-//     $conn = new DbConn(driver: $_ENV["DSN_DRIVER"], serverName: $_ENV["DATABASE_HOSTNAME"], userName: $_ENV["DATABASE_USERNAME"], password: $_ENV["DATABASE_PASSWORD"], database: $databaseName);
-//     $conn = new DbTableOp($conn->getConnection());
-//     return $conn;
-// }
-
-// With MVC Design Pattern
 use Dotenv\Dotenv;
 use db\Connection\DbConn;
 use db\Connection\DbTable;
