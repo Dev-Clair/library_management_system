6ecc0ba6f71ed872a4a33987e7aab7843bfe92fb
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-07-27 15:16:42 +0100

    feat: setting up views

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ try {
     $router = new Router();
 
     $router->get('/UserController', [app\Controller\UserController::class, 'index'])
-        ->get('/UserController/index', [app\Controller\UserController::class, 'index'])
-        ->get('/UserController/create', [app\Controller\UserController::class, 'create'])
-        ->post('/UserController/store', [app\Controller\UserController::class, 'store'])
-        ->get('/UserController/edit', [app\Controller\UserController::class, 'edit'])
-        ->post('/UserController/store', [app\Controller\UserController::class, 'update']);
+        ->get('/UserController', [app\Controller\UserController::class, 'index'])
+        ->get('/UserController', [app\Controller\UserController::class, 'create'])
+        ->post('/UserController', [app\Controller\UserController::class, 'store'])
+        ->get('/UserController', [app\Controller\UserController::class, 'edit'])
+        ->post('/UserController', [app\Controller\UserController::class, 'update']);
 
     $result = $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
     echo $result;
---
+++ src/app/Views/components/layout.php
@@ -0,0 +1,13 @@
+<!-- views/user/index.php -->
+<?php
+require_once __DIR__ . '/../components/head.php';
+?>
+
+<?php
+require_once __DIR__ . '/../components/status.php';
+require_once __DIR__ . '/../components/pageContent.php';
+?>
+
+<?php
+require_once __DIR__ . '/../components/footer.php';
+?>
